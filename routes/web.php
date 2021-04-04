3c85aa91f90193137f945be68e5060ca1944acf9
Zero Blitz <raffibaihaqy02@gmail.com>
2021-04-04 14:38:38 +0700

    Roles

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/admin','AdminController@index');
+Route::get('/user','UserController@index');
